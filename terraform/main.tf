69cc7bf9052e1f7fab7c5c306350c64394faf897
Ninkovski <ninkovski03@gmail.com>
2025-12-27 00:53:46 -0500

    feat(tf): enable serverless capability for Cosmos DB account

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,6 @@ resource "azurerm_cosmosdb_account" "cv_data" {
   resource_group_name = azurerm_resource_group.core.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
-  free_tier_enabled   = true
 
   consistency_policy {
     consistency_level = "Session"
@@ -77,6 +76,9 @@ resource "azurerm_cosmosdb_account" "cv_data" {
     failover_priority = 0
   }
 
+  capabilities {
+    name = "EnableServerless"
+  }
 }
 
 output "storage_account_name" {
